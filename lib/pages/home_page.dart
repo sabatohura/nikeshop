80edfde301b275dfc6f1a3c43e1858ad615674ed
sabatohura <sabato@hura.one>
2023-09-22 08:03:17 +0200

    Add Bottom navbars

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:nikeshop/components/bottom_nav_bar.dart';
 
-class HomePage extends StatelessWidget {
+import 'cart_page.dart';
+import 'shop_page.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _page = [
+    const ShopPage(),
+    const CartPage(),
+  ];
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _page[_selectedIndex],
+    );
   }
 }
